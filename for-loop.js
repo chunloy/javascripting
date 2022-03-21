bfc30f630955d9b744f3cbc0df2668786d0eecf7
Aaron Au <aaron.au@me.com>
2022-03-21 04:22:31 +0000

    Initial commit with all my work so far

---
+++ for-loop.js
@@ -0,0 +1,7 @@
+let total = 0;
+const limit = 10;
+for(let i = 0; i < limit; i++) {
+  //keep a running total
+  total += i;
+}
+console.log(total);
---
+++ object-keys.js
@@ -0,0 +1,8 @@
+const car = {
+  make: 'Honda',
+  model: 'Accord',
+  year: 2020
+}
+//return array of keys from car object
+const keys = Object.keys(car);
+console.log(keys);
